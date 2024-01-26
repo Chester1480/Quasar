3728d27e67a4df89f713b885b3b316b0072c9732
chester <chester0148@gmail.com>
2024-01-27 00:13:34 +0800

    init project

---
+++ src/layouts/LineLiffMainLayout.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+
+const linksList = [
+  {
+    title: '輸入資料',
+    caption: '',
+    icon: 'mdi-database',
+    link: ''
+  },
+]
+
+export default defineComponent({
+  name: 'LineLiffMainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  },
+  async mounted (){
+
+  }
+})
+</script>
